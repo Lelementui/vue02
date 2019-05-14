c04ca051aaf16dec2028ae6a60045a58b8f38813
Your Name <you@example.com>
2019-05-14 15:19:27 +0800

    操作header

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+     <v-header :seller="seller"></v-header>
+  </div>
+</template>
+
+<script>
+import {urlParse} from './common/js/util';
+import header from './components/header/header.vue'
+const response=require('./common/data/seller.json');
+const ERR_OK = 0;
+export default {
+  // name: 'App',
+  data(){
+      seller: {
+          id: (() => {
+            let queryParam = urlParse();
+            return queryParam.id;
+          })()
+      }
+  },
+  components: {
+     'v-header':header
+  },
+  created(){
+        if (response.errno === ERR_OK) {
+          this.seller = Object.assign({}, this.seller, response.data);
+        }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,38 @@
+
+<template>
+   <div class="header">
+       <div class="header_content">
+            <div class="header_left">
+                <img width="64" height="64" :src="seller.avatar" alt="">
+            </div>
+           <div class="header_right">
+               <div class="shop-name">{{seller.name}}</div>
+           </div>
+       </div>
+   </div>
+</template>
+
+<script>
+export default {
+    data(){
+      return{
+
+      }
+    },
+    props:{
+        seller:{
+            type:Object
+        }
+    }
+}
+</script>
+
+<style>
+.header{
+    width:100%;background-color: rgba(7,17,27,0.5);height:130px;
+}
+.header img{padding:22px 0 0 15px}
+.header_left{width:30%;float: left;box-sizing: border-box}
+.header_right{width:70%;float: right;box-sizing: border-box;padding:22px 0 0 10px;}
+.header_right .shop-name{}
+</style>
